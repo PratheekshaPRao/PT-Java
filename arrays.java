02ef771e2ce9e86ee4bcbfad3f2718e7a1f4e5d7
PratheekshaPRao <Pratheeksha.23cs101@sode-edu.in>
2025-04-08 12:04:12 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,17 @@
+
+public class arrays {
+    public static void main(String [] args){
+        int[] arr=new int[5];
+        arr[0]=11;
+        arr[1]=12;
+        arr[2]=13;
+        arr[3]=14;
+        arr[4]=15;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+        
+    }
+}
---
+++ arry3.java
@@ -0,0 +1,12 @@
+
+public class arry3 {
+
+    public static void main(String[] args) {
+        int[] arr = {11, 12, 13, 14};
+        System.out.println(arr[0]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[7]);
+
+    }
+}

f0ab33b77fff77f6c7850e3cf684eb4c5b95baf2
PratheekshaPRao <Pratheeksha.23cs101@sode-edu.in>
2025-04-11 10:01:11 +0530

    Add files via upload

---
+++ num4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SMV
+ */
+public class num4 {
+    public static void main(String[]args){
+        int n=5;
+        int sq=n*n;
+        if(sq%10==n)
+        {
+            System.out.println("automorphic");
+        }
+        else{
+            System.out.println("not");
+        }
+        
+    }
+}

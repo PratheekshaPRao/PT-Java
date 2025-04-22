c650e03172aff597c85205bf00abccc7883e27cf
PratheekshaPRao <Pratheeksha.23cs101@sode-edu.in>
2025-04-22 11:43:18 +0530

    Add files via upload

---
+++ pen.java
@@ -0,0 +1,13 @@
+
+package mod2;
+public class pen {
+  int price;
+  pen()
+  {
+      price=5;
+      System.out.println(price);
+  }
+  public static void main(String[] args){
+      pen p=new pen();
+  }
+}
